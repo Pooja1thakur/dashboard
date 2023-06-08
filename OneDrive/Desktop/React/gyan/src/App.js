1a4be175d1c2517ca357ee7200e41b9778b76380
Pooja1thakur <pooja2810rohit@gmail.com>
2023-06-09 01:37:00 +0530

    admin page

---
+++ OneDrive/Desktop/React/gyan/src/App.js
@@ -0,0 +1,41 @@
+
+
+import'./App.css';
+import Header from './component/Header';
+import Movie from './component/Movie';
+import movies from './movie.json';
+
+function App (){
+
+
+    return (
+      <div className="App">
+    
+        <Header/>
+    
+    <div className='main'>
+         {
+          movies.map((element,index) =>
+            {
+                return(
+           <Movie 
+           key={index}
+           title={element.Title} 
+           year={element.Year}
+           img={element.Poster}/>
+                )
+            })
+        }
+
+      
+
+    
+    </div>
+      </div>
+    );
+  }
+  
+
+  export default App;
+
+
